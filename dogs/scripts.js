cf49f95ee656c52058e44e93f221e40102c02d8f
Louise Dolan <lousd@live.co.uk>
2022-07-08 15:10:54 +0100

    imageds

--- dogs/scripts.js
+++ dogs/scripts.js
@@ -23,6 +23,9 @@ request.onload = function () {
       const h1 = document.createElement('h1');
       h1.textContent = movie.title;
 
+      const img = document.createElement('img')
+      img.src = movie.movie_banner
+
       const p = document.createElement('p');
       movie.description = movie.description.substring(0, 300);
       p.textContent = `${movie.description}...`;
@@ -30,7 +33,9 @@ request.onload = function () {
       container.appendChild(card);
       card.appendChild(h1);
       card.appendChild(p);
-    });
+      card.appendChild(img);
+
+    })
   } else {
     const errorMessage = document.createElement('marquee');
     errorMessage.textContent = `Gah, it's not working!`;
